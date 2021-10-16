407fe077c717d1a601a400ec2cae9be964058c13
Kunal Bansal <kunalbansal@Kunals-MacBook-Air.local>
2021-10-16 14:19:17 +0530

    Email Authentication completed

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,12 +1,11 @@
 const User = require('../models/users');
 const jwt = require('jsonwebtoken');
-
+let alert = require('alert'); 
 const activatekey = 'accountactivatekey123';
 const clientURL = 'http://localhost:8000';
 
 const mailgun = require("mailgun-js");
-
-const mg = mailgun({apiKey: process.env.PRIVATE_API_KEY, domain: process.env.DOMAIN});
+const mg = mailgun({apiKey: process.env.MAILGUN_APIKEY , domain: process.env.DOMAIN});
 
 
 module.exports.emailAuth = function(req,res){
@@ -15,7 +14,7 @@ module.exports.emailAuth = function(req,res){
     console.log(name , email, password, confirmPassword);
     console.log(req.body);
     if( password != confirmPassword){
-        alert('Password and Confirm Password should be same');
+        alert("Password and Confirm Password should be same");
         return res.redirect('back');
     }
     
@@ -25,9 +24,9 @@ module.exports.emailAuth = function(req,res){
             return res.redirect('back');
         }
         
-        if(!user){
+        if(!user){ 
 
-            const token = jwt.sign({name, email, password},activatekey,{expiresIn : '20m'});
+            const token = jwt.sign({name, email, password, confirmPassword},activatekey,{expiresIn : '20m'});
 
             const data = {
                 from: 'noreply@student.com',
@@ -35,16 +34,15 @@ module.exports.emailAuth = function(req,res){
                 subject: 'Account Activation Key',
                 html : `
                     <h2>Please click  on below link to activate your account</h2>
-                    <a href="${clientURL}/authentication/activate/${token}">CLICK HERE</a>
-                    
+                    <a href="${clientURL}/authentication/activate?token=${token}">CLICK HERE</a>
                 `
             };
             mg.messages().send(data, function (error, body) {
                 if(error){
-                    console.log(error.message, 'Email has not sent');
+                    console.log(error.message);
                     return res.redirect('back');
                 }
-                console.log('Email has been sent for verification');
+                console.log('Email has been sent for vecrification');
                 return res.redirect('back');    
             });
 
@@ -59,30 +57,38 @@ module.exports.emailAuth = function(req,res){
             // });
         }else{
             console.log('User with this email already exist!!');
+            return res.redirect('back');
         }
     });
 }
 
 module.exports.activateAccount = function(req,res){
-    const token = req.body.token;
+    const token = req.query.token;
+    console.log(token);
     if(token){
         jwt.verify(token,activatekey, function(err, decodedToken){
             if(err){
                 console.log('Incorrect or expire link');
                 return res.redirect('back');
             }
-            const{name , email , password} = decodedToken;
+            const{name , email , password, confirmPassword} = decodedToken;
+            var user1 = new User();
+            user1.name = name;
+            user1.email = email;
+            user1.password = password;
+            user1.confirmPassword = confirmPassword;
             User.findOne({email : email}, function(err , user){
                 if(err){
                     console.log('Error in finding user in Sign-in ');
                 }
                 
                 if(!user){
-                    User.create(req.body,function(err,user){
+                    User.create(user1,function(err,user){
                         if(err){
-                            console.log('Error in creating a user while accoun activation');
+                            console.log('Error in creating a user while account activation', err);
                             return res.redirect('back');
                         }
+                        return res.redirect('/');
                     });
                 }else{
                     console.log("SignUp successfully!!");
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,6 @@ const authController = require('../controller/auth-controller');
 router.get('/',homeController.home);
 router.get('/sign-up',signUpController.signUp);
 router.post('/auth',authController.emailAuth);
-router.post('/email-activate',authController.activateAccount)
+router.get('/authentication/activate',authController.activateAccount)
 
 module.exports = router;
